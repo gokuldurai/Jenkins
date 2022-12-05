6f85dbf43fba17f15bd5c8f191786158f5c8ab6f
welcome <welcome@DESKTOP-1KB0RC3>
2022-12-05 16:25:10 +0530

    jenkins commit 1

---
+++ src/test/java/JenkinsPractice/BuildCreationTest.java
@@ -0,0 +1,22 @@
+package JenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class BuildCreationTest {
+	
+	
+	@Test
+	public void smokeBuildTest() {
+		
+		System.out.println("smoke");
+		
+	}
+	
+	@Test
+	public void regBuildTest() {
+		
+		System.out.println("regression");
+		
+	}
+
+}
---
+++ src/test/java/JenkinsPractice/BuildDeploymentTest.java
@@ -0,0 +1,21 @@
+package JenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class BuildDeploymentTest {
+	@Test
+	public void sample1Test() {
+		
+		System.out.println("samplefirst");
+		
+	}
+	
+	@Test
+	public void sample2Test() {
+		
+		System.out.println("samplesecond");
+		
+	}
+
+
+}
